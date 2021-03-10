705f617bb8e99c42b845692a0fbcf1045a9cf414
Antoine Gourlay <antoine@gourlay.fr>
2021-03-10 20:49:28 +0100

    pretty print attributes

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use anyhow::*;
-use firmconfig::cli::ProgramOptions;
-use std::path::PathBuf;
+use firmconfig::{cli::ProgramOptions, Attribute, AttributeType};
+use std::{
+    io::{stdout, Write},
+    path::PathBuf,
+};
 use structopt::StructOpt;
 
 fn main() -> Result<()> {
@@ -13,8 +16,58 @@ fn main() -> Result<()> {
             let attributes = firmconfig::list_attributes(&path)?;
 
             for a in attributes {
-                println!("{:?}", a);
+                print_attribute(&a)?;
             }
+        }
+    }
+
+    Ok(())
+}
+
+fn print_attribute(a: &Attribute) -> Result<()> {
+    let out = stdout();
+    let mut f = out.lock();
+    writeln!(f, "{}", a.name)?;
+    writeln!(f, "    Name: {}", a.display_name)?;
+    match a.tpe {
+        AttributeType::Integer { min, max, step } => {
+            writeln!(f, "    Type: Integer")?;
+            writeln!(f, "        Min: {}", min)?;
+            writeln!(f, "        Max: {}", max)?;
+            writeln!(f, "        Step: {}", step)?;
+        }
+        AttributeType::String {
+            min_length,
+            max_length,
+        } => {
+            writeln!(f, "    Type: String")?;
+            writeln!(f, "        Min: {}", min_length)?;
+            writeln!(f, "        Max: {}", max_length)?;
+        }
+        AttributeType::Enumeration {
+            ref possible_values,
+        } => {
+            writeln!(f, "    Type: Enumeration")?;
+            writeln!(f, "        Possible Values:")?;
+            for p in possible_values {
+                writeln!(f, "            {}", p)?;
+            }
+        }
+    }
+    match &a.current_value {
+        Ok(v) => {
+            writeln!(f, "    Current value: {}", v)?;
+        }
+        Err(_) => {
+            writeln!(f, "    Current value: <Access Denied>")?;
+        }
+    }
+    match &a.default_value {
+        Ok(v) => {
+            writeln!(f, "    Default value: {}", v)?;
+        }
+        Err(_) => {
+            writeln!(f, "    Default value: <Access Denied>")?;
         }
     }
 
